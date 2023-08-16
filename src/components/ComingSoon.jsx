70ebf0ea955b55a8a930802477faac1a115be526
Anirudh <anirudh.panchangamranganath@thws.study.de>
2023-08-17 01:57:21 +0200

    new changes/responsive

--- src/components/ComingSoon.jsx
+++ src/components/ComingSoon.jsx
@@ -1,61 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Marquee from "react-fast-marquee";
 import useSound from 'use-sound';
 import music from '../assets/Olepash_-_Easy_Joy.mp3'
-
+import { HiVolumeUp } from 'react-icons/hi'
 
 
 
 
 const ComingSoon = () => {
-  const[play,{ stop }] = useSound(music, {volume: 0.025});
+  const[play,{ stop }] = useSound(music, {volume: 0.010});
   return (
-    <div className='bg-[#282828] relative h-screen w-screen flex items-center justify-center'>
-        
-        
-        <button className='text-white text-[256px] font-bold font-ubuntu
-                         tracking-[0.40em] absolute w-screen 
-                          min-h-min text-center items-center flex-col' 
-                          onMouseEnter={() => play()} onMouseLeave={() => !stop()}>
-                            
-                            
-                            LUMAURA
-                            
-                            
-                            
-        </button>
-      
+    <div className='flex flex-col h-screen w-screen justify-between items-center'>
 
-        <div className='w-screen bg-[#1d1d1d] h-[2px] absolute top-[10%]'>
-        </div>
-        
-        <div className='w-screen bg-[#1d1d1d] h-[2px] absolute top-[13%]'>
-        </div>
+      <div className='text-[#757575] mt-[5rem] w-screen border-t-2 border-b-2 border-[#1e1e1e]'>
+        <Marquee autoFill='true'>
+          <span className='px-7'> COMING SOON </span>
+          <span className='px-7'> UNDER MAINTAINANCE </span>
+          <span className='px-7'> COMING SOON </span>
+          <span className='px-7'> UNDER MAINTAINANCE </span>
+        </Marquee>
+      </div>
 
-        <div className='text-[#757575] absolute text-[20px] top-[10%] w-screen '>
-         <Marquee autoFill="true"> 
-            <span> COMING SOON &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;</span>
-            <span> UNDER MAINTAINANCE&emsp;&emsp;&emsp;&emsp;&emsp;&emsp; </span>
-            <span> COMING SOON &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;</span>
-            <span> UNDER MAINTAINANCE &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;</span>
-          </Marquee>
-        </div>
+      <p className='text-white font-ubuntu tracking-[0.4em] w-auto m-auto
+                        sm:text-[5rem] md:text-[9rem] lg:text-[11rem] xl:text-[15rem]
+                        font-bold text-center flex justify-center items-center
+                        '>
+        LUMAURA
+      </p>
 
-        <div className='w-screen bg-[#1d1d1d] h-[2px] absolute top-[90%]'>
-        </div>
-        
-        <div className='w-screen bg-[#1d1d1d] h-[2px] absolute top-[87%]'>
-        </div>
-
-        <div className='text-[#757575] absolute text-[20px] top-[87%] w-screen '>
-         <Marquee autoFill="true" direction='right'> 
-            <span> COMING SOON &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;</span>
-            <span> UNDER MAINTAINANCE&emsp;&emsp;&emsp;&emsp;&emsp;&emsp; </span>
-            <span> COMING SOON &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;</span>
-            <span> UNDER MAINTAINANCE &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;</span>
-          </Marquee>
-        </div>
-
+      <div className='text-[#757575] mb-[5rem] w-screen border-t-2 border-b-2 border-[#1e1e1e]'>
+        <Marquee autoFill='true'  direction='right'> 
+          <span className='px-7'> COMING SOON </span>
+          <span className='px-7'> UNDER MAINTAINANCE </span>
+          <span className='px-7'> COMING SOON </span>
+          <span className='px-7'> UNDER MAINTAINANCE </span>
+        </Marquee>
+      </div>
     </div>
   )
 }
